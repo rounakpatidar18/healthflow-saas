2319ee26935a37330a2fcac1eb94fd31a9774510
Rounak <rounaksatyam@gmail.com>
2026-05-02 17:47:10 +0530

    Add authentication routes for signup and login

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::AuthController < ApplicationController
+  def signup
+    user = User.new(user_params)
+
+    if user.save
+      token = Auth::JsonWebToken.encode(user_id: user.id)
+      render json: { token: token }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def login
+    user = User.find_by(email: params[:email])
+
+    if user&.authenticate(params[:password])
+      token = Auth::JsonWebToken.encode(user_id: user.id)
+      render json: { token: token }, status: :ok
+    else
+      render json: { error: "Invalid credentials" }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation, :role, :tenant_id)
+  end
+end
