09ab81f90bc5301736a5b1c0356a30f8a85b2279
Rounak <rounaksatyam@gmail.com>
2026-05-02 16:29:43 +0530

    Add Tenant model and associate users with tenants

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
+  belongs_to :tenant
   enum role: { admin: 0, doctor: 1, staff: 2 }
 
   validates :email, presence: true, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_02_105145) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_02_105808) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "tenants", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.string "subdomain"
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
