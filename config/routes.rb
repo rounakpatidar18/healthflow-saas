62e151652a95b079054939cdfc6f105290a7d8e9
Rounak <rounaksatyam@gmail.com>
2026-05-03 18:06:17 +0530

    add health check endpoint for monitoring

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require "sidekiq/web"
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => "/sidekiq"
-
+  get "/health", to: proc { [ 200, {}, [ "OK" ] ] }
   namespace :api do
     namespace :v1 do
       post "signup", to: "auth#signup"
