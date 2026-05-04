b75fa2d81a2b96a0cbea8dfa8d3ee925d8d102e3
Rounak <rounaksatyam@gmail.com>
2026-05-04 12:19:12 +0530

    setup routing with login and dashboard pages

--- app/frontend/entrypoints/application.jsx
+++ app/frontend/entrypoints/application.jsx
@@ -1,14 +1,19 @@
+// app/frontend/entrypoints/application.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../src/pages/Login";
+import Dashboard from "../src/pages/Dashboard";
 
 function App() {
   return (
-    <div>
-      <h1>HealthFlow SaaS</h1>
-      <p>Frontend is running 🚀</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-const container = document.getElementById("root");
-createRoot(container).render(<App />);
+createRoot(document.getElementById("root")).render(<App />);
