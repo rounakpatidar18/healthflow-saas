985cd5cef570e3ff36260b16e9dfc70ef9c7c3cb
Rounak <rounaksatyam@gmail.com>
2026-05-04 11:43:22 +0530

    Integrate React frontend with Rails view using Vite

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
+  root "home#index"
+  get "home/index"
   mount Rswag::Ui::Engine => "/api-docs"
   mount Rswag::Api::Engine => "/api-docs"
   mount Sidekiq::Web => "/sidekiq"
