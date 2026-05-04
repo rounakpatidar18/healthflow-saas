4a70ac278eb834b2be76667fb80e84b7895f851c
Rounak <rounaksatyam@gmail.com>
2026-05-04 12:23:08 +0530

    add protected route for authenticated dashboard access

--- app/frontend/entrypoints/application.jsx
+++ app/frontend/entrypoints/application.jsx
@@ -1,16 +1,27 @@
-// app/frontend/entrypoints/application.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "../src/pages/Login";
 import Dashboard from "../src/pages/Dashboard";
+import ProtectedRoute from "../src/components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Public route */}
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* Protected route */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
